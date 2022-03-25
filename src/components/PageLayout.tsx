b498a46482b840c47a9363a71af4e3674507ffcb
Ишемгулов Арслан <arslan.ishemgulov@skbkontur.ru>
2022-03-25 23:18:07 +0500

    add PageLayout

---
+++ src/components/PageLayout.tsx
@@ -0,0 +1,16 @@
+import React, {ReactNode} from 'react';
+import './PageLayout.css'
+
+type Props = {
+    children: ReactNode
+}
+
+const PageLayout: React.FC<Props> = ({children}) => {
+    return (
+        <main className='main'>
+            {children}
+        </main>
+    );
+}
+
+export default PageLayout
