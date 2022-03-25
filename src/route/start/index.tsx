e4892ea192a0497812b6ec9ad4932eb5337f37ca
Ишемгулов Арслан <arslan.ishemgulov@skbkontur.ru>
2022-03-25 23:18:52 +0500

    add navigation and minor styles

--- src/route/start/index.tsx
+++ src/route/start/index.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import {useNavigate} from 'react-router-dom';
+import logo from '../../assets/logo.png';
+import './start.css'
 
-const StartWindow: () => JSX.Element = () => {
-  return (
-    <div>
-      <h1>Start page</h1>
-    </div>
-  )
+const StartWindow: React.FC = () => {
+    return (
+        <div>
+            <h1>Кто хочет стать миллионером?</h1>
+            <div className="rowContainer">
+                <img alt="game logo" width={251} src={logo}/>
+            </div>
+            <div className="rowContainer">
+                <StartGameButton>Начать игру</StartGameButton>
+            </div>
+        </div>
+    )
+}
+
+const StartGameButton: React.FC = props => {
+    const navigate = useNavigate();
+    return <button onClick={() => navigate('/game')} {...props}/>
 }
 
 export default StartWindow
