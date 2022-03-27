b36ae47921e716784c8aea6ded8a6b5371c236f6
Misha Pryadun <pryadun99@icloud.com>
2022-03-28 00:08:25 +0500

    fix(questions): update logic

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -1,57 +1,205 @@
-import {IQuestionsType} from "../types/questionType";
-
 //Вопросы брал здесь
 //http://gameshows.ru/wiki/%D0%9A%D1%82%D0%BE_%D1%85%D0%BE%D1%87%D0%B5%D1%82_%D1%81%D1%82%D0%B0%D1%82%D1%8C_%D0%BC%D0%B8%D0%BB%D0%BB%D0%B8%D0%BE%D0%BD%D0%B5%D1%80%D0%BE%D0%BC%3F_(%D0%9F%D0%BE%D0%B2%D1%82%D0%BE%D1%80_%D0%B2%D0%BE%D0%BF%D1%80%D0%BE%D1%81%D0%B0)
 
-const QUESTIONS: IQuestionsType = {
-  easy: {
-    0: {
-      title: 'Как называется популярный рецепт приготовления макарон с мясом?',
-      answer: {
-        0: 'По-деревенски',
-        1: 'По-флотски',
-        2: 'По-братски',
-        3: 'По-божески',
-      },
-      rightAnswer: 1
-    },
-    1: {
-      title: 'Что потерял Ослик Иа в сказке про Винни-Пуха?',
-      answer: {
-        0: 'Память',
-        1: 'Совесть',
-        2: 'Зубы',
-        3: 'Хвост',
-      },
-      rightAnswer: 3
-    },
-  },
-  medium: {
-    0: {
-      title: 'Кольцо какого цвета на олимпийском флаге символизирует Европу?',
-      answer: {
-        0: 'Жёлтого',
-        1: 'Голубого',
-        2: 'Красного',
-        3: 'Зелёного',
-      },
-      rightAnswer: 1
-    },
-  },
-  hard: {
-    0: {
-      title: 'Что не входило в рецепт салата оливье в 1894 году?',
-      answer: {
-        0: 'Картофель',
-        1: 'Огурцы',
-        2: 'Докторская колбаса',
-        3: 'Оливки',
-      },
-      rightAnswer: 1
-    },
-  }
+export enum QuestionDifficult {
+  easy,
+  medium,
+  hard
 }
 
+type TAnswerNumbers = 0 | 1 | 2 | 3
+
+interface IQuestions {
+  id: number;
+  difficult: QuestionDifficult;
+  text: string;
+  variants: [string, string, string, string];
+  rightAnswer: TAnswerNumbers
+}
+
+const Questions: IQuestions[] = [
+  {
+    id: 1000,
+    difficult: QuestionDifficult.easy,
+    text: 'Как называется популярный рецепт приготовления макарон с мясом?',
+    variants: [
+      'По-деревенски',
+      'По-флотски',
+      'По-братски',
+      'По-божески'
+    ],
+    rightAnswer: 1
+  },
+  {
+    id: 1001,
+    difficult: QuestionDifficult.easy,
+    text: 'Что потерял Ослик Иа в сказке про Винни-Пуха?',
+    variants: [
+      'Память',
+      'Совесть',
+      'Зубы',
+      'Хвост'
+    ],
+    rightAnswer: 3
+  },
+  {
+    id: 1002,
+    difficult: QuestionDifficult.easy,
+    text: 'Какое слово пропущено в названии пиратского флага «… Роджер»?',
+    variants: [
+      'Весёлый',
+      'Одноногий',
+      'Солёный',
+      'Непотопляемый'
+    ],
+    rightAnswer: 0
+  },
+  {
+    id: 1003,
+    difficult: QuestionDifficult.easy,
+    text: 'Что является характеристикой коллекционного вина?',
+    variants: [
+      'Стойкость',
+      'Выдержка',
+      'Выносливость',
+      'Трезвость'
+    ],
+    rightAnswer: 1
+  },
+  {
+    id: 1004,
+    difficult: QuestionDifficult.easy,
+    text: 'Чему равна сумма углов треугольника?',
+    variants: [
+      '90 градусов',
+      '100 градусов',
+      '180 градусов',
+      '360 градусов'
+    ],
+    rightAnswer: 2
+  },
+  {
+    id: 2000,
+    difficult: QuestionDifficult.medium,
+    text: 'Кольцо какого цвета на олимпийском флаге символизирует Европу?',
+    variants: [
+      'Жёлтого',
+      'Голубого',
+      'Красного',
+      'Зелёного'
+    ],
+    rightAnswer: 1
+  },
+  {
+    id: 2001,
+    difficult: QuestionDifficult.medium,
+    text: 'Как заканчивается первая строчка считалочки: «Аты-баты…»?',
+    variants: [
+      'Шли солдаты',
+      'Плыли моряки',
+      'Скакали кавалеристы',
+      'Кричали депутаты'
+    ],
+    rightAnswer: 0
+  },
+  {
+    id: 2002,
+    difficult: QuestionDifficult.medium,
+    text: 'Из меха какого животного шьют шапки королевских гвардейцев Великобритании?',
+    variants: [
+      'Соболь',
+      'Медведь',
+      'Лиса',
+      'Лама'
+    ],
+    rightAnswer: 1
+  },
+  {
+    id: 2003,
+    difficult: QuestionDifficult.medium,
+    text: 'В каком виде спорта разыгрывается Кубок Стэнли?',
+    variants: [
+      'Футбол',
+      'Теннис',
+      'Велоспорт',
+      'Хоккей'
+    ],
+    rightAnswer: 3
+  },
+  {
+    id: 2004,
+    difficult: QuestionDifficult.medium,
+    text: 'Какой напиток получают из сахарного тростника?',
+    variants: [
+      'Джин',
+      'Коньяк',
+      'Ром',
+      'Кальвадос'
+    ],
+    rightAnswer: 2
+  },
+  {
+    id: 3000,
+    difficult: QuestionDifficult.hard,
+    text: 'Что не входило в рецепт салата оливье в 1894 году?',
+    variants: [
+      'Картофель',
+      'Огурцы',
+      'Докторская колбаса',
+      'Оливки'
+    ],
+    rightAnswer: 1
+  },
+  {
+    id: 3001,
+    difficult: QuestionDifficult.hard,
+    text: 'В каком конструкторском бюро создали самолёт АНТ-25, на котором экипажи Чкалова и Громова достигли США?',
+    variants: [
+      'Туполева',
+      'Антонова',
+      'Ильюшина',
+      'Лавочкина'
+    ],
+    rightAnswer: 0
+  },
+  {
+    id: 3002,
+    difficult: QuestionDifficult.hard,
+    text: 'Какой камень венчает Большую императорскую корону Российской империи, хранящуюся в Алмазном фонде?',
+    variants: [
+      'Алмаз',
+      'Сапфир',
+      'Благородная шпинель',
+      'Рубин'
+    ],
+    rightAnswer: 2
+  },
+  {
+    id: 3003,
+    difficult: QuestionDifficult.hard,
+    text: 'Какой была настоящая фамилия Штирлица, если верить книгам Юлиана Семёнова?',
+    variants: [
+      'Исаев',
+      'Тихонов',
+      'Петров',
+      'Владимиров'
+    ],
+    rightAnswer: 3
+  },
+  {
+    id: 3004,
+    difficult: QuestionDifficult.hard,
+    text: 'С какой фигуры начинаются соревнования по городошному спорту?',
+    variants: [
+      'Часовые',
+      'Артиллерия',
+      'Пулемётное гнездо',
+      'Пушка'
+    ],
+    rightAnswer: 3
+  },
+]
+
 export function getQuestions() {
-  return QUESTIONS
+  return Questions
 }
--- src/route/game/index.tsx
+++ src/route/game/index.tsx
@@ -1,20 +1,53 @@
 import React from "react";
-import {getQuestions} from "../../data/questions";
+import {getQuestions, QuestionDifficult} from "../../data/questions";
+
+const passQuestions: number[] = []
+
+
+type TgetQuestion = [questionText: string, questionVariants: [string, string, string, string], questionId: number]
+
+const questions = getQuestions()
+const getRandom = (length: number = 1) => {
+  return Math.floor(Math.random() * length)
+}
+
+const getQuestion = (difficult: QuestionDifficult, passQuestions?: number[]): TgetQuestion => {
+  const questionsCollection = questions
+    .filter(question => question.difficult === difficult)
+    .filter(question => !passQuestions?.includes(question.id))
+
+  const question = questionsCollection[getRandom(questionsCollection.length)]
+
+  return [question.text, question.variants, question.id]
+}
+
+const checkAnswer = (id: number, index: number): boolean =>  {
+  const question = questions.filter(question => question.id === id)[0]
+  console.log(index === question?.rightAnswer)
+  return index === question?.rightAnswer
+}
+
+const [questionText, questionVariants, questionId]: TgetQuestion = getQuestion(QuestionDifficult.easy, passQuestions)
+passQuestions.push(questionId)
 
 const GameWindow: () => JSX.Element = () => {
-
-  const QUESTIONS = getQuestions()
-  const question = QUESTIONS.easy[0].title
-  const answerFirst = QUESTIONS.easy[0].answer[0]
-
   return (
     <div>
       <h2>Game Window</h2>
       <div className="question__title">
-        {question}
+        {questionText}
       </div>
-      <button className="question__answer-btn">
-        {answerFirst}
+      <button className="question__answer-btn" onClick={()=>checkAnswer(questionId,0)}>
+        {questionVariants[0]}
+      </button>
+      <button className="question__answer-btn" onClick={()=>checkAnswer(questionId,1)}>
+        {questionVariants[1]}
+      </button>
+      <button className="question__answer-btn" onClick={()=>checkAnswer(questionId,2)}>
+        {questionVariants[2]}
+      </button>
+      <button className="question__answer-btn" onClick={()=>checkAnswer(questionId,3)}>
+        {questionVariants[3]}
       </button>
     </div>
   )
