a17c01f74097f12ca0661b44cd10e9672791ad14
Ишемгулов Арслан <arslan.ishemgulov@skbkontur.ru>
2022-04-04 23:47:12 +0500

    add Timer on game page

--- src/route/game/index.tsx
+++ src/route/game/index.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import { QuestionDifficult } from '../../data/questions';
-import { checkAnswer, getRandomQuestion, TgetQuestion } from '../../components/questions/';
+import React, {useState} from 'react';
+import {QuestionDifficult} from '../../data/questions';
+import {checkAnswer, getRandomQuestion, TgetQuestion} from '../../components/questions/';
 import '../../components/questions/style.css';
+import Timer from "../../components/Timer/Timer";
 
 const passQuestions: number[] = [];
 const firstQuestion: TgetQuestion = getRandomQuestion(QuestionDifficult.easy, passQuestions);
@@ -9,9 +10,9 @@ const firstQuestion: TgetQuestion = getRandomQuestion(QuestionDifficult.easy, pa
 const GameWindow: React.FC = () => {
 	const [[questionText, questionVariants, questionId], setQuestion] = useState(firstQuestion);
 
-	const handelClick = (index: number) => {
-		const isRigth = checkAnswer(questionId, index);
-		if (!isRigth) {
+	const handleClick = (index: number) => {
+		const isRight = checkAnswer(questionId, index);
+		if (!isRight) {
 			console.log('Не правильно!');
 			return;
 		}
@@ -19,29 +20,37 @@ const GameWindow: React.FC = () => {
 			passQuestions.length < 4
 				? QuestionDifficult.easy
 				: passQuestions.length < 9
-				? QuestionDifficult.medium
-				: QuestionDifficult.hard;
+					? QuestionDifficult.medium
+					: QuestionDifficult.hard;
 		passQuestions.push(questionId);
 		setQuestion(getRandomQuestion(difficult, passQuestions));
 	};
 
 	return (
-		<div className="question">
-			<div className="question__container--title">
-				<div className="question__item--title">
-					<div className="question__title">{questionText}</div>
-				</div>
+		<div>
+			<div className="timer__container">
+				<Timer paused={false} duration={30}
+					   onTimeExpiration={() => console.log("expired")}/>
 			</div>
-			<div className="question__container">
-				{questionVariants.map((variant) => {
-					return (
-						<div className="question__item">
-							<button className="question__answer-btn" onClick={() => handelClick(variant.id)}>
-								{variant.text}
-							</button>
-						</div>
-					);
-				})}
+
+			<div className="question">
+				<div className="question__container--title">
+					<div className="question__item--title">
+						<div className="question__title">{questionText}</div>
+					</div>
+				</div>
+				<div className="question__container">
+					{questionVariants.map((variant) => {
+						return (
+							<div className="question__item">
+								<button className="question__answer-btn"
+										onClick={() => handleClick(variant.id)}>
+									{variant.text}
+								</button>
+							</div>
+						);
+					})}
+				</div>
 			</div>
 		</div>
 	);
