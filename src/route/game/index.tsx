e79a66e061ce01bdca3a88a9dcb92fe7dce0413c
Ишемгулов Арслан <arslan.ishemgulov@skbkontur.ru>
2022-04-10 23:29:32 +0500

    Revert "add Timer on game page"
    This reverts commit a17c01f74097f12ca0661b44cd10e9672791ad14.

--- src/route/game/index.tsx
+++ src/route/game/index.tsx
@@ -1,8 +1,7 @@
-import React, {useState} from 'react';
-import {QuestionDifficult} from '../../data/questions';
-import {checkAnswer, getRandomQuestion, TgetQuestion} from '../../components/questions/';
+import React, { useState } from 'react';
+import { QuestionDifficult } from '../../data/questions';
+import { checkAnswer, getRandomQuestion, TgetQuestion } from '../../components/questions/';
 import '../../components/questions/style.css';
-import Timer from "../../components/Timer/Timer";
 
 const passQuestions: number[] = [];
 const firstQuestion: TgetQuestion = getRandomQuestion(QuestionDifficult.easy, passQuestions);
@@ -10,9 +9,9 @@ const firstQuestion: TgetQuestion = getRandomQuestion(QuestionDifficult.easy, pa
 const GameWindow: React.FC = () => {
 	const [[questionText, questionVariants, questionId], setQuestion] = useState(firstQuestion);
 
-	const handleClick = (index: number) => {
-		const isRight = checkAnswer(questionId, index);
-		if (!isRight) {
+	const handelClick = (index: number) => {
+		const isRigth = checkAnswer(questionId, index);
+		if (!isRigth) {
 			console.log('Не правильно!');
 			return;
 		}
@@ -20,37 +19,29 @@ const GameWindow: React.FC = () => {
 			passQuestions.length < 4
 				? QuestionDifficult.easy
 				: passQuestions.length < 9
-					? QuestionDifficult.medium
-					: QuestionDifficult.hard;
+				? QuestionDifficult.medium
+				: QuestionDifficult.hard;
 		passQuestions.push(questionId);
 		setQuestion(getRandomQuestion(difficult, passQuestions));
 	};
 
 	return (
-		<div>
-			<div className="timer__container">
-				<Timer paused={false} duration={30}
-					   onTimeExpiration={() => console.log("expired")}/>
+		<div className="question">
+			<div className="question__container--title">
+				<div className="question__item--title">
+					<div className="question__title">{questionText}</div>
+				</div>
 			</div>
-
-			<div className="question">
-				<div className="question__container--title">
-					<div className="question__item--title">
-						<div className="question__title">{questionText}</div>
-					</div>
-				</div>
-				<div className="question__container">
-					{questionVariants.map((variant) => {
-						return (
-							<div className="question__item">
-								<button className="question__answer-btn"
-										onClick={() => handleClick(variant.id)}>
-									{variant.text}
-								</button>
-							</div>
-						);
-					})}
-				</div>
+			<div className="question__container">
+				{questionVariants.map((variant) => {
+					return (
+						<div className="question__item">
+							<button className="question__answer-btn" onClick={() => handelClick(variant.id)}>
+								{variant.text}
+							</button>
+						</div>
+					);
+				})}
 			</div>
 		</div>
 	);
