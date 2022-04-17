da04a860a7dbf5dadaaca598b127643c7f454d1e
Ишемгулов Арслан <arslan.ishemgulov@skbkontur.ru>
2022-04-17 22:21:28 +0500

    refactor Timer

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,37 +1,34 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import './Timer.css';
 
 interface Props {
 	paused: boolean;
 	duration: number;
 
-	onTimeExpiration?(): void;
+	onTimeExpiration(): void;
 
-	onPause?(secondsLeft: number): void;
+	onPause(secondsLeft: number): void;
 }
 
 const Timer: React.FC<Props> = (timerProps) => {
 	const { paused, duration, onTimeExpiration, onPause } = timerProps;
 	const [seconds, setSeconds] = useState(duration);
 	const timer = useRef<number | undefined>();
+	const clearInterval = useCallback((timer) => {
+		if (timer.current) window.clearInterval(timer.current);
+		timer.current = undefined;
+	}, []);
 
 	useEffect(() => {
 		if (seconds <= 0) {
-			onTimeExpiration && onTimeExpiration();
-		} else if (paused) {
-			onPause && onPause(seconds);
+			onTimeExpiration();
 		} else {
-			timer.current = window.setInterval(() => setSeconds((v) => v - 1), 1000);
-			console.log(timer.current + ' started');
+			paused
+				? onPause(seconds)
+				: (timer.current = window.setInterval(() => setSeconds((v) => v - 1), 1000));
 		}
-		return () => {
-			if (timer.current) {
-				window.clearInterval(timer.current);
-				console.log(timer.current + ' cleared');
-				timer.current = undefined;
-			}
-		};
-	}, [seconds, paused, onTimeExpiration, onPause]);
+		return () => clearInterval(timer);
+	}, [seconds, onTimeExpiration, clearInterval, paused, onPause]);
 
 	return <div className="timer">{seconds}</div>;
 };
